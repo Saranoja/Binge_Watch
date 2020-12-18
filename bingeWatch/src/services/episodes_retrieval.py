4a35cc46b1d7e56b6336147f933019afc1e8cf90
Saranoja <Saranoja@users.noreply.github.com>
2020-12-18 21:17:28 +0200

    fix: updated the ep retrieval to return all aired episodes list

--- bingeWatch/src/services/episodes_retrieval.py
+++ bingeWatch/src/services/episodes_retrieval.py
@@ -1,10 +1,13 @@
 import requests
 import json
+from datetime import datetime
 from bingeWatch.src.services.tvmaze_scraper import get_series_id_from_name
 
 BASE_URL = "http://api.tvmaze.com/shows/"
 ROUTE = "/episodes"
 
+
+# TODO: map json to contain only episode name, season, number and airdate + filter result to have airdate < current_date
 
 def get_last_episode(series_name):
     series_id = get_series_id_from_name(series_name)
@@ -14,11 +17,12 @@ def get_last_episode(series_name):
     response = requests.request("GET", url)
 
     all_episodes = json.loads(response.text)
-    episode_season = all_episodes[len(all_episodes) - 1].get("season")
-    episode_number = all_episodes[len(all_episodes) - 1].get("number")
-    last_episode = f"S{episode_season}E{episode_number}"
 
-    print(last_episode)
+    aired_episodes = [ep for ep in all_episodes if
+                      ep['airdate'] != "" and datetime.strptime(ep['airdate'], "%Y-%m-%d") < datetime.today()]
 
+    relevant_fields = ["name", "season", "number", "airdate"]
+    relevant_details_dict = [{key: value for key, value in episode.items() if key in relevant_fields} for episode in
+                             aired_episodes]
 
-get_last_episode("when they see us")
+    return relevant_details_dict
