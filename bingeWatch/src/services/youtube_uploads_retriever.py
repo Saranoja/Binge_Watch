d8dfd6e2860abe452b41f0b63785be5254fc1906
Saranoja <Saranoja@users.noreply.github.com>
2020-12-26 14:29:46 +0200

    fix: updated youtube retriever to support playlists

--- bingeWatch/src/services/youtube_uploads_retriever.py
+++ bingeWatch/src/services/youtube_uploads_retriever.py
@@ -9,10 +9,12 @@ YOUTUBE_VIDEOS_BASE_URL = "https://www.youtube.com/watch?v="
 
 
 def get_uploads_for_episode(series_name, episode_season, episode_number, max_results=5):
-    search_query = f"q={series_name}%20S{episode_season}E{episode_number}%20trailer"
+    search_query = f"q={series_name}%20S{episode_season}E{episode_number}"
     url = f"{API_BASE_URL}{API_KEY}&{PART}&{MAX_RESULTS}{max_results}&{search_query}"
     response = requests.request("GET", url)
     youtube_uploads = json.loads(response.text)
-    youtube_ids = map(lambda video: video["id"]["videoId"], youtube_uploads["items"])
+    youtube_ids = map(
+        lambda video: video["id"]["videoId"] if video["id"]["kind"] == "youtube#video" else video["id"]["playlistId"],
+        youtube_uploads["items"])
     youtube_urls = map(lambda id: f'{YOUTUBE_VIDEOS_BASE_URL}{id}', youtube_ids)
     return list(youtube_urls)
