652824df9dbc6d59241498c22720c05f58e5aa0c
Saranoja <Saranoja@users.noreply.github.com>
2021-01-04 21:31:32 +0200

    feat: added readme and updated setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Binge Watch
+
+BingeWatch is a tool to monitor the favourite TV shows.
+When run, the program will list all episodes that haven't been watched yet, ordered by a specific score the user has previously set.
+
+Another functionality the tool offers is the possibility to search on YouTube for trailers and other uploads that are related to a certain episode of a TV show.
+
+Other possible commands are: snooze a specific show, add or delete one, update its score, update the last seen episode.
--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BingeWatch_Saranoja",
+    version="1.0.1",
+    author="Calin Irina",
+    author_email="contact@irinacalin.codes",
+    description="A tool to monitor the favourite TV shows",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Saranoja/Binge_Watch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
